bf6efd66baab88532e6b62101940656942a423c8
Sheldon <100086149@students.swinburne.edu.my>
2019-10-03 17:13:14 +0800

    Added navigation bar

--- app/src/main/java/com/example/staffcafeposapp/MainActivity.java
+++ app/src/main/java/com/example/staffcafeposapp/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.staffcafeposapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.staffcafeposapp.Fragments.MenuFragment;
+import com.example.staffcafeposapp.Fragments.OrdersFragment;
+import com.example.staffcafeposapp.Fragments.TablesFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()) {
+                case R.id.navigation_menu:
+                    selectedFragment = new MenuFragment();
+                    break;
+                case R.id.navigation_orders:
+                    selectedFragment = new OrdersFragment();
+                    break;
+                case R.id.navigation_tables:
+                    selectedFragment = new TablesFragment();
+                    break;
+            }
+
+            assert selectedFragment != null;
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+
+            return true;
+        }
+    };
 }
+
